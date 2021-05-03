0749719ab70ffed12c9244fc8be4373b8fdea7ef
Liz Thompson <thompsonel2011@gmail.com>
2021-05-03 16:37:02 -0400

    list of cities and autocomplete

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,24 @@
 var searchBtnEl = document.querySelector(".searchBtn");
 var currentDate =  moment().add(10, 'days').calendar();
 console.log(currentDate);
-var cityList = [];
+var cityList = []; //lists the cities for autocomplete
 
+
+// Function to create the cityList 
 $.getJSON("./assets/city.list.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
+    console.log(json); 
+    var city = '';
+    var state = ''
+    for (let index = 0; index < json.length; index++) {
+        cityList.push(`${json[index].name}, ${json[index].state}`)
+    }
 });
-
+// Function to autocomplete based on cityList 
+$( function() {
+        $( ".searchInput" ).autocomplete({
+      source: cityList
+    });
+  } );
 
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 

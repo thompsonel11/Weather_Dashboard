284c2033367984bdb99ec81655836cac60bdf805
Liz Thompson <thompsonel2011@gmail.com>
2021-05-04 15:58:54 -0400

    this may get worse before it gets better

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,41 @@
-var searchBtnEl = document.querySelector(".searchBtn");
-var currentDate =  moment().add(10, 'days').calendar();
+var searchBtnEl = $("#searchBtn");
+var currentDate =  moment().add(0, 'days').calendar();
 console.log(currentDate);
 var cityList = []; //lists the cities for autocomplete
+var searchInputEl = $(".searchInput")
 
 
 // Function to create the cityList 
 $.getJSON("./assets/city.list.json", function(json) {
     console.log(json); 
-    var city = '';
-    var state = ''
     for (let index = 0; index < json.length; index++) {
         cityList.push(`${json[index].name}, ${json[index].state}`)
     }
 });
+
 // Function to autocomplete based on cityList 
 $( function() {
         $( ".searchInput" ).autocomplete({
-      source: cityList
+        source: cityList
     });
   } );
 
+// // Function to go get data (API) for user input
+$('#searchBtn').on('click', function () {
+var userSearch = 
+
+
+// Pseudo Code
+// 1. create a list of cities for autocomplete/select 
+// 2. utilize the API to go get the information about the city searched 
+//     - today's weather and 5 day forecast 
+// 3. save today's weather from search to the local storage so that data can be called when user selects the button from history
+
+
+
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+// https://openweathermap.org/api/geocoding-api
 
 // key: 08ecca64f6fc6837576d8b780463552f
 

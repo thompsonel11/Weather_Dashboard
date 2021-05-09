9d5943eeed803c1d23b368cfa348b769102b9326
Liz Thompson <thompsonel2011@gmail.com>
2021-05-09 11:58:05 -0400

    CSS

--- assets/script.js
+++ assets/script.js
@@ -6,54 +6,49 @@ var searchInputEl = $(".searchInput");
 var currentForecastEl = $(".currentForecast");
 var recentSearches = [];
 
-// TODO: populate recentSearches with data in local storage
-buildRecentSearchButtons()
-
 //-----------------------------------------------------------------
-// AUTOCOMPLETE
-// Function to create the cityList
+// Autocomplete: Functions used to add autocomplete feature utilizing local file cityList
 $.getJSON("./assets/city.list.json", function (json) {
   for (let index = 0; index < json.length; index++) {
     cityList.push(`${json[index].name}, ${json[index].state}`);
   }
 });
-// Function to autocomplete based on cityList
+
 $(function () {
   $(".searchInput").autocomplete({
     source: cityList,
   });
 });
 //-----------------------------------------------------------------
-// APIs
-// Function to go get data via APIs based on user input
+// Click event that takes user input, pushes to resent search and runs search using the function below
 searchBtnEl.click(function () {
   const userSearch = $(".searchInput").val();
   pushToRecentSearches(userSearch);
   console.log(userSearch);
-  
-  // Promise used to fetch response from APIs and display data
   runSearch(userSearch);
   buildRecentSearchButtons();
 });
 
+// RUN SEARCH FUNCTION BEGINS
+// runSearch function first splits the user's input into City, State 
 function runSearch(userSearch) {
-
   const userSearchSplit = userSearch.split(",");
   console.log(userSearchSplit);
-
   // Define city and state as a result of the split
   const cityName = userSearchSplit[0].trim();
   const stateCode = userSearchSplit[1].trim();
-
+  localStorage.setItem('cityNameLocal',JSON.stringify(cityName))
+  // If statemenmt to require user to enter a city AND state 
   if (userSearchSplit.length < 2) {
     alert("Please enter a valid City and State");
     return;
   }
-  // Define variables for both API's: Current and 5 Day Forecast
+  // Define constants for API Key and for both API's: Current and 5 Day Forecast
   const apiKey = '08ecca64f6fc6837576d8b780463552f';
   const currentAPI = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode},US&appid=${apiKey}`;
   const fiveDayAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${stateCode},US&appid=${apiKey}`;
 
+  // Utilizing Promise to return both APIs via fetch  
   Promise.all([fetch(currentAPI), fetch(fiveDayAPI)])
     .then(function (responses) {
       return Promise.all(
@@ -69,28 +64,24 @@ function runSearch(userSearch) {
       displayFiveDay(data[1]);
     });
 }
-
+//-----------------------------------------------------------------
+//Function created for pushing recent searches by creating a search string with limit of 8
 function pushToRecentSearches(searchString) {
-  const searchLimit = 3;
-
+  const searchLimit = 8;
   if(recentSearches.find(search => search === searchString)) return;
-
   if (recentSearches.length < searchLimit) {
     recentSearches.push(searchString);
   } else {
     const searchesWithoutOldest = recentSearches.slice(1, searchLimit);
     recentSearches = [...searchesWithoutOldest, searchString];
   }
-
-  //  TODO save to local storage
 }
-
+// Buildimg the buttons for search history with click events that will present user with data.
 function buildRecentSearchButtons() {
   const recentSearchesEl = $(".recentSearches");
   recentSearchesEl.empty();
-  
+
   for (let i = 0; i < recentSearches.length; i++) {
-    
     const btn = $(`<button type="button" class="btn btn-secondary">${recentSearches[i]}</button>`)
     btn.click(function() {
       runSearch(recentSearches[i])
@@ -98,9 +89,8 @@ function buildRecentSearchButtons() {
     recentSearchesEl.append(btn);
   }
 }
-
 //-----------------------------------------------------------------
-// FUNCTION TO DISPLAY CURRENT WEATHER
+// Function to dynamically build and display current data card 
 function buildWeatherElement(data) {
   currentForecastEl.empty();
   const cardContainer = $('<div class = "card border border-primary">');
@@ -113,34 +103,22 @@ function getWeatherCard(weatherData, cityName, cardStyle) {
   const info = `${cityName} ${date}`;
   const convertedTemp = Math.floor((weatherData.main.temp - 273.15) * 1.8 + 32);
   const title = $('<h4 class = "card-title">').text(info);
-
-  const temp = $('<p class = "card-text">').text(
-    "Temperature: " + convertedTemp.toFixed() + " °F"
-  );
-  const wind = $('<p class = "card-text">').text(
-    "Wind: " + weatherData.wind.speed + " MPH"
-  );
-  const humidity = $('<p class = "card-text">').text(
-    "Humidity: " + weatherData.main.humidity + " %"
-  );
+  const temp = $('<p class = "card-text">').text( "Temperature: " + convertedTemp.toFixed() + " °F");
+  const wind = $('<p class = "card-text">').text("Wind: " + weatherData.wind.speed + " MPH");
+  const humidity = $('<p class = "card-text">').text("Humidity: " + weatherData.main.humidity + " %");
   const cardBody = $(`<div class = "${cardStyle}">`);
-  const icon = $('<img class = "weatherIcon">').attr(
-    "src",
-    "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png"
-  );
+  const icon = $('<img class = "weatherIcon">').attr("src","https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png");
 
   title.append(icon);
   cardBody.append(title, temp, wind, humidity);
-
   return cardBody;
 }
 
-// FUNCTION TO DISPLAY 5 DAY FORECAST
+// Function to dynamically build and display 5 day forecast data cards
 function displayFiveDay(data) {
   const fiveDayForecastEl = $(".fiveDayForecast");
   fiveDayForecastEl.empty();
   fiveDayForecastEl.append("<h2>Five Day Forecast</h2>");
-
   const cardContainer = $('<div class = "row">');
 
   for (let index = 7; index < 40; index += 8) {
@@ -148,6 +126,5 @@ function displayFiveDay(data) {
       getWeatherCard(data.list[index], data.city.name, "col-md-2 border")
     );
   }
-
   fiveDayForecastEl.append(cardContainer);
 }
